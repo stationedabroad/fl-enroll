668b5a20c5f5d7dc6520b3c2c1bfb4ed4afda3bd
sulman shaikh <sulmanmh@gmail.com>
2020-04-04 11:45:52 +0100

    images and templatea added

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,7 @@
 from application import app
+from flask import render_template
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "<h1>Server test one ...</h1>"
+    return render_template("index.html")
