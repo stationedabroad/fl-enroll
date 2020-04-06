4df01b8017cfa164981c9977b3793058085f7a46
sulman shaikh <sulmanmh@gmail.com>
2020-04-06 15:41:11 +0100

    courses list added as template code

--- application/routes.py
+++ application/routes.py
@@ -9,7 +9,12 @@ def index():
 
 @app.route("/courses")
 def courses():
-    return render_template("courses.html", login=False)
+    course_data =  [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, 
+                    {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, 
+                    {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, 
+                    {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, 
+                    {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
+    return render_template("courses.html", course_data=course_data)
 
 @app.route("/register")
 def register():
