425aad7f2cf74115c9fd1991f52b0956b72d7306
sulman shaikh <sulmanmh@gmail.com>
2020-04-06 15:57:58 +0100

    amended menu highlighting from JS to jinja template

--- application/routes.py
+++ application/routes.py
@@ -5,7 +5,7 @@ from flask import render_template
 @app.route("/index")
 @app.route("/home")
 def index():
-    return render_template("index.html", login=False)
+    return render_template("index.html", index=True)
 
 @app.route("/courses")
 def courses():
@@ -14,12 +14,12 @@ def courses():
                     {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, 
                     {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, 
                     {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
-    return render_template("courses.html", course_data=course_data)
+    return render_template("courses.html", course_data=course_data, courses=True)
 
 @app.route("/register")
 def register():
-    return render_template("register.html", login=False)
+    return render_template("register.html", register=True)
 
 @app.route("/login")
 def login():
-    return render_template("login.html", login=False)            
+    return render_template("login.html", login=True)            
